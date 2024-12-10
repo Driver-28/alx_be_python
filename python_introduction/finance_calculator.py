39aa70436f58b592e7ab5364ba7b631a53361fe6
Zelalem <zzele57@gmail.com>
2024-12-10 23:12:15 +0300

    user's monthly savings and potential future savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter your monthly income:"))
 num2 = float(input("Enter your total monthly expenses:"))
-'Monthly Savings' = num1 - num2
-'Projected Savings' = 'Monthly Savings' * 12 + ('Monthly Savings' * 12 * 0.05)
-print("Your monthly savings are $",'Monthly Savings'.)
-print("project savings after one year, with interest, is: $",'Projected Savings'.)
+Monthly_Savings = num1 - num2
+Projected_Savings = Monthly_Savings * 12 + Monthly_Savings * 12 * 0.05
+print("Your monthly savings are $",Monthly_Savings.)
+print("project savings after one year, with interest, is: $",Projected_Savings.)
