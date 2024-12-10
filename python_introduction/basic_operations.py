07b677f2a787260e2ee3b81bac7a08a8c116f60e
Zelalem <zzele57@gmail.com>
2024-12-10 21:57:52 +0300

    add, subtract, and multiplication program

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 number1 = 10
 number1 = 5
-Addition = number1 - number2
+Addition = number1 + number2
 Subtraction = number1 - number2
 Multiplication = number1 * number2
 print("Addition of", number1, "and", number2, "is", Addition)
