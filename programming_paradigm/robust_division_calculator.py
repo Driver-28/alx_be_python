d551df4b69bd1645060a2bb65b1850b20b6108ba
Zelalem <zzele57@gmail.com>
2025-01-19 15:05:39 +0300

    robust division have try except calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,12 +1,18 @@
 class divide:
     def __init__(self, numerator, denominator):
-        self.numerator = float(numerator)
-        self.denominator = float(denominator)
+        try:
+            self.numerator = float(numerator)
+            self.denominator = float(denominator)
+            if self.denominator == 0:
+                print("Error: Cannot divide by zero.")
+                self.denominator = 1.0
+        except ValueError:
+            print("Error: please enter numeric values only.")
+            self.numerator = 0.0
+            self.denominator = 1.0
     def safe_divide(self):
         try:
             result = self.numerator / self.denominator
             print(f"The result of the division is {result}")
         except ZeroDivisionError:
             print("Error: Cannot divide by zero.")
-        except ValueError:
-            print("Error: Please enter numeric values only.")
