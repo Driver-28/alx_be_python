250e1f5e544b64534b0912049e3006f80cd30643
Zelalem <zzele57@gmail.com>
2025-01-19 10:32:48 +0300

    robust division have try except calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,9 @@
 class calculator:
     def safe_divide(self, numerator, denominator):
         try:
-            self.a = float (a)
-            self.b = float (b)
-            result = a / b
+            self.a = float (numerator)
+            self.b = float (denominator)
+            result = numerator / denominator
             print(f"The result of the division is {result}")
         except ZeroDivisionError:
             print("Error: Cannot divide by zero.")
