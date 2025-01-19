f93ce3d7e8cf60e96eb917a503a099d7fd4d382a
Zelalem <zzele57@gmail.com>
2025-01-19 16:29:49 +0300

    unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(4,5),20)
         self.assertEqual(self.calc.multiply(0,10),0)
         self.assertEqual(self.calc.multiply(-3,5),-15)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,2),5.0)
         self.assertEqual(self.calc.divide(9,3),3.0)
         self.assertEqual(self.calc.divide(5,0))
