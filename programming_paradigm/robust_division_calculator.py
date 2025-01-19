66c74d4e06aef18306053d59272ed294c730fcc1
Zelalem <zzele57@gmail.com>
2025-01-19 15:50:30 +0300

    robust division have try except calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
-        denom float(denominator)
+        denom = float(denominator)
             if denom == 0:
                 return "Error: Cannot divide by zero."
             return f"The result of the division is {num / denom}"
