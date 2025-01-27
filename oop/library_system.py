9ed9aead367ccbd0a73a54cfd8a6e72fbf4b20d2
Zelalem <zzele57@gmail.com>
2025-01-27 14:42:08 +0300

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,5 +20,7 @@ class  Library:
     def __init__(self, books):
         self.books = []
     def add_book(self, book):
-
+        self.books.append(book)
     def list_books(self):
+        for book in self.books:
+            print (book)
