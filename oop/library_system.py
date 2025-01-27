776d4930248a03fdfdbc1a08cb41dbd8e785a0de
Zelalem <zzele57@gmail.com>
2025-01-27 17:00:31 +0300

    Library Management System

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,7 +1,7 @@
 class Book:
     def __init__(self, title, author):
-        self.title=title
-        self.author=author
+        self.title = title
+        self.author = author
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 class EBook(Book):
@@ -18,7 +18,7 @@ class PrintBook(Book):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class  Library:
     def __init__(self):
-        self.books=[]
+        self.books = []
     def add_book(self, book):
         self.books.append(book)
     def list_books(self):
