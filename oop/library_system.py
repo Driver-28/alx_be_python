ab4e7b44c23214f4be925d750b7298a4f381cb4c
Zelalem <zzele57@gmail.com>
2025-01-27 16:33:04 +0300

    Library Management System

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    def __str__(str):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.file_size}"
 class  Library:
     def __init__(self, books):
