4d99aee05cdd41e5f9b6de0609c3bf609756c244
Zelalem <zzele57@gmail.com>
2025-01-28 13:06:52 +0300

    polymorphism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -5,7 +5,7 @@ class Shape:
         self.width = width
         raise NotImplementedError("Subclass must implement this method")
 class Rectangle(Shape):
-    def __init__(self, length, width):
+    def area(self, length, width):
         return self.length * self.width
 class Circle(Shape):
     def __init__(self, radius):
