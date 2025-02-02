b095370bfd270018316c2a6f986d462e162b6b49
Zelalem <zzele57@gmail.com>
2025-02-02 21:43:15 +0300

    book store

--- control-flow/pattern_drawing.py
+++
@@ -1,7 +0,0 @@
-pattern=int(input("Enter the size of the pattern:"))
-row = 0
-while row < pattern:
-    for i in range(pattern):
-        print("*", end="")
-    print()
-    row +=1
--- python_introduction/future_age_calculator.py
+++
@@ -1,4 +0,0 @@
-year = 2023
-user = int(input("How old are you?"))
-age = year + user
-print("In", age,", you will be", user, "years old.")
