498ed8dded0228db14f74e40b396917d2bcf4bdb
Zelalem <zzele57@gmail.com>
2025-02-07 13:53:44 +0300

    python introduction

---
+++ finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = float(input("Enter your monthly income:"))
+monthly_expenses = float(input("Enter your total monthly expenses:"))
+Monthly_Savings = monthly_income - monthly_expenses
+Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
+print("Your monthly savings are $",Monthly_Savings,".")
+print("project savings after one year, with interest, is: $",Projected_Savings,".")
---
+++ future_age_calculator.py
@@ -0,0 +1,4 @@
+year = 2023
+user = int(input("How old are you?"))
+age = year + user
+print("In", age,", you will be", user, "years old.")
